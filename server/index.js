6f107e25525c804d0d049f17d22aec5d61953757
JRIngram <jr-ingram@hotmail.co.uk>
2022-01-22 10:31:33 +0000

    Add linter and fix issues
    Fix non-alcoholic imports and change port

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ const DB_URL = `mongodb://${DB_HOST}/${DB_PORT}`;
 const DB_NAME = 'cocktail_db';
 const DB_COLLECTION_NAME = 'cocktails';
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => {
-    const htmlPage = `
+  const htmlPage = `
         <h1>Cocktail Raffle!</h1>
         <h2>API Docs - V1</h2>
         <ul>
@@ -21,70 +21,72 @@ app.get('/', (req, res) => {
                 <p>e.g: /cocktail/random/ingredients?ingredient=Vermouth&ingredient=Sugar Syrup</p>
             </li>
         </ul>
-    `
-    res.send(htmlPage);
-})
+    `;
+  res.send(htmlPage);
+});
 
 app.get('/cocktail/random', async (req, res) => {
-    try {
-        const client = await MongoClient.connect(DB_URL);
-        const db = client.db(DB_NAME);
-        const collection = db.collection(DB_COLLECTION_NAME);
-        const results = await collection.aggregate([{ $sample: { size: 1 } }]).toArray();
-        res.status(200);
-        res.send(results);
-    } catch (err) {
-        console.error(err);
-        res.status(500);
-        res.send({})
-    }
+  try {
+    const client = await MongoClient.connect(DB_URL);
+    const db = client.db(DB_NAME);
+    const collection = db.collection(DB_COLLECTION_NAME);
+    const results = await collection.aggregate([{ $sample: { size: 1 } }]).toArray();
+    res.status(200);
+    res.send(results);
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+    res.send({});
+  }
 });
 
 app.get('/cocktail/random/ingredients', async (req, res) => {
-    const { ingredient } = req.query;
-    try {
-        let ingredientList;
-        if(ingredient && typeof ingredient === 'string'){
-            ingredientList = [ingredient];
-        } else {
-            ingredientList = [...ingredient];
-        }
-
-        ingredientList = ingredientList.map((ingredient) => {
-            return ingredient.charAt(0).toUpperCase() + ingredient.slice(1);
-        })
-
-        const client = await MongoClient.connect(DB_URL);
-        const db = client.db(DB_NAME);
-        const collection = db.collection(DB_COLLECTION_NAME);
-        
-        // returns one result which contains all of the parameters queried by
-        let results = await collection.aggregate([
-            { $match: { "ingredients.ingredient": { $all: ingredientList } } },
-            { $sample: { size: 1 } },
-        ]).toArray();
-
-        if (results.length > 0) {
-            let result = results[0]
-            // calculate percentage of ingredients needed vs ingredients you have
-            const percentageMatch = ingredientList.length / result.ingredients.length;
-            result = {
-                ...result,
-                percentageMatch
-            }
-            res.status(200);
-            res.send(result);
-        } else {
-            res.status(200);
-            res.send({});
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500);
-        res.send([])
+  const { ingredient } = req.query;
+  try {
+    let ingredientList;
+    if (ingredient && typeof ingredient === 'string') {
+      ingredientList = [ingredient];
+    } else {
+      ingredientList = [...ingredient];
     }
+
+    ingredientList = ingredientList.map((ingredientListItem) => {
+      const upperCaseIngredient = ingredientListItem
+        .charAt(0).toUpperCase() + ingredientListItem.slice(1);
+      return upperCaseIngredient;
+    });
+
+    const client = await MongoClient.connect(DB_URL);
+    const db = client.db(DB_NAME);
+    const collection = db.collection(DB_COLLECTION_NAME);
+
+    // returns one result which contains all of the parameters queried by
+    const results = await collection.aggregate([
+      { $match: { 'ingredients.ingredient': { $all: ingredientList } } },
+      { $sample: { size: 1 } },
+    ]).toArray();
+
+    if (results.length > 0) {
+      let result = results[0];
+      // calculate percentage of ingredients needed vs ingredients you have
+      const percentageMatch = ingredientList.length / result.ingredients.length;
+      result = {
+        ...result,
+        percentageMatch,
+      };
+      res.status(200);
+      res.send(result);
+    } else {
+      res.status(200);
+      res.send({});
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+    res.send([]);
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Cocktail Raffle server listening on port ${port}`)
-})
+  console.log(`Cocktail Raffle server listening on port ${port}`);
+});
